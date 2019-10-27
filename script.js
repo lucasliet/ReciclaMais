27e05a41c1dd9064d6d1933dd9976e87039899ab
lucasliet <lucasouliveira@gmail.com>
2019-10-27 20:30:29 -0300

    CleanCode

--- script.js
+++ script.js
@@ -9,91 +9,84 @@ function navbar() {
     )
 }
 
-function suggest() {
+function populaLista(){
     var lista = new Array;
 
     //checa se o cache ta vazio, caso não esteja puxa o cache pra arraylist
-    if (localStorage.getItem('listaLocais') != null) {
-        lista.push(JSON.parse(localStorage.getItem('listaLocais')));
+    if (localStorage.getItem("listaLocais") != null) {
+                   //converte a String do JSON no localStorage pra array
+        lista.push(JSON.parse(localStorage.getItem("listaLocais")));
     }
 
+    return lista;
+}
+
+function suggest() {
+    var lista = new Array;
+    lista = populaLista();
+
+    //puxa os elementos dos campos digitados na página pra um objeto
     var obj = new Object;
-
-    //puxa os elementos dos campos digitados na página pro objeto
-    obj.nomePessoa = document.getElementById("inputNome").value;
-    obj.nomeLocal  = document.getElementById("inputLugar").value;
-    obj.endereco   = document.getElementById("inputCEP").value;
-    obj.info       = document.getElementById("comentario").value;
-
-    obj.plastico      = document.getElementById("plastico").checked;
-    obj.vidro         = document.getElementById("vidro").checked;
-    obj.metal         = document.getElementById("metal").checked;
-    obj.papel         = document.getElementById("papel").checked;
-    obj.bateria       = document.getElementById("bateria").checked;
-    obj.eletronicos   = document.getElementById("eletronicos").checked;
+    obj = {
+        nomePessoa    : document.getElementById("inputNome").value,
+        nomeLocal     : document.getElementById("inputLugar").value,
+        endereco      : document.getElementById("inputCEP").value,
+        info          : document.getElementById("comentario").value,
+        
+        plastico      : document.getElementById("plastico").checked,
+        vidro         : document.getElementById("vidro").checked,
+        metal         : document.getElementById("metal").checked,
+        papel         : document.getElementById("papel").checked,
+        bateria       : document.getElementById("bateria").checked,
+        eletronicos   : document.getElementById("eletronicos").checked,
+    }
 
     //manda o objeto pra arraylist
     lista.push(obj);
 
-    //manda a arraylist pro cache
-    localStorage.setItem('listaLocais', JSON.stringify(lista));
+    //manda a arraylist pro cache      //converte a array pra String pro JSON do localStorage
+    localStorage.setItem("listaLocais", JSON.stringify(lista));
     
-    // Gerar Lista no Log do navegador
+    //gera Lista no Log do navegador
     for (var i = 0; i < lista.length; i++){
         console.log(lista[i]);
     }
     
-    alert('Local armazenado com sucesso!');
+    alert("Local armazenado com sucesso!");
 }
 
 //função de teste pra gerar a lista num código html
-function lista(){
-    var lista = new Array;
+function listaHTML(){
+    var lista = populaLista();
 
-    if (localStorage.getItem('listaLocais') != null) {
-        lista.push(JSON.parse(localStorage.getItem('listaLocais')));
-    }
-
-    var html = '<h1>Lista de Locais</h1> <br><br><br>'; 
-
+    var html = "<h1>Lista de Locais</h1> <br><br><p>"; 
     for (var i = 0; i < lista.length; i++){
-        let nomePessoa  = lista[i].nomePessoa;
-        let nomeLocal   = lista[i].nomeLocal;
-        let endereco    = lista[i].endereco;
-        let info        = lista[i].info;
-        let plastico    = lista[i].plastico;
-        let vidro       = lista[i].vidro;
-        let metal       = lista[i].metal;
-        let papel       = lista[i].papel;
-        let bateria     = lista[i].bateria;
-        let eletronicos = lista[i].eletronicos;
+        html += "Nome: "            + lista[i].nomePessoa + "<br>"
+        html += "Estabelecimento: " + lista[i].nomeLocal  + "<br>"
+        html += "Endereço: "        + lista[i].endereco   + "<br>"
+        html += "Descrição: "       + lista[i].info       + "<br>"
+        html += "Tipos de Lixo: <br>"
         
-        html += 'Nome: ' + nomePessoa + '<br>'
-        html += 'Estabelecimento: ' + nomeLocal + '<br>'
-        html += 'Endereço: ' + endereco + '<br>'
-        html += 'Descrição: ' + info + '<br>'
-        html += 'Tipos de Lixo: <br>'
-        
-        if(plastico){
-            html+= 'Plástico ||'
+        if(lista[i].plastico){
+            html+= "Plástico ||"
         }
-        if(vidro){
-            html+= 'Vidro ||'
+        if(lista[i].vidro){
+            html+= "Vidro ||"
         }
-        if(metal){
-            html+= 'Metal ||'
+        if(lista[i].metal){
+            html+= "Metal ||"
         }
-        if(papel){
-            html+= 'Papel ||'
+        if(lista[i].papel){
+            html+= "Papel ||"
         }
-        if(bateria){
-            html+= 'Bateria ||'
+        if(lista[i].bateria){
+            html+= "Bateria ||"
         }
-        if(eletronicos){
-            html+= 'Eletrônicos ||'
+        if(lista[i].eletronicos){
+            html+= "Eletrônicos ||"
         }
 
-        html +=    '____________________________________<br>';
+        html += "____________________________________</p><br>";
     }
 
     document.write(html);
