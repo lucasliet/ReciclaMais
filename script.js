a67607ab1b01935de2e678dd49db548327a61178
lucasliet <lucasouliveira@gmail.com>
2019-10-28 20:45:00 -0300

    Lista Style e comentários

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function navbar() {
+    //escreve o código da navbar no html
     document.write(`<ul>
     <a href="index.html"><button class="logo">R+</button></a>
     <li><a href="index.html">Página Inicial</a></li>
@@ -61,35 +62,39 @@ function suggest() {
 function listaHTML(){
     var lista = populaLista();
 
-    var html = "<h1>Lista de Locais</h1><br><p>"; 
+    //cria String que vai ser passado pro código html
+    var html = "<h1>Lista de Locais</h1><br>"; 
+    //passa por cada objeto da array e coloca as variaveis nessa String
     for (var i = 0; i < lista.length; i++){
-        html += "Nome: "            + lista[i].nomePessoa + "<br>"
-        html += "Estabelecimento: " + lista[i].nomeLocal  + "<br>"
-        html += "Endereço: "        + lista[i].endereco   + "<br>"
-        html += "Descrição: "       + lista[i].info       + "<br>"
+        html += "<p>Nome: "            + lista[i].nomePessoa + "<br>"
+        html += "Estabelecimento: "    + lista[i].nomeLocal  + "<br>"
+        html += "Endereço: "           + lista[i].endereco   + "<br>"
+        html += "Descrição: "          + lista[i].info       + "<br>"
         html += "Tipos de Lixo: <br>"
         
+        //checa se cada tipo de lixo foi marcado, se sim escreve ele na String
         if(lista[i].plastico){
-            html+= "Plástico || "
+            html+= "<img src='images/plastico.png'> Plástico "
         }
         if(lista[i].vidro){
-            html+= "Vidro || "
+            html+= "<img src='images/vidro.png'> Vidro "
         }
         if(lista[i].metal){
-            html+= "Metal || "
+            html+= "<img src='images/metal.png'> Metal "
         }
         if(lista[i].papel){
-            html+= "Papel || "
+            html+= "<img src='images/papel.png'> Papel "
         }
         if(lista[i].bateria){
-            html+= "Bateria || "
+            html+= "<img src='images/bateria.png'> Bateria "
         }
         if(lista[i].eletronicos){
-            html+= "Eletrônicos || "
+            html+= "<img src='images/eletronicos.png'> Eletrônicos "
         }
-
-        html += "<br>____________________________________</p><br>";
+        //html += "<br><br><button class='btn btn-primary ml-2'>Deletar Sugestão</button>"
+        html += "<br>____________________________________</p>";
     }
-
+    
+    //escreve a String no html
     document.write(html);
 }
