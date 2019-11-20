9b7cc2d1cf51fe9676184d78c76a5c1fc9004841
lucasliet <lucasouliveira@gmail.com>
2019-11-20 00:43:12 -0300

    Nova Logo teste

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //escreve o código da navbar no html
 var menu = `
     <nav class="navbar navbar-light menu navbar-expand-lg p-0 my-3 mx-2 shadow">
-        <a class="ml-3 navbar-brand" href="index.html"><button class="logo">R+</button></a>
+        <a class="ml-3 navbar-brand" href="index.html">
+            <img class="logo" src="images/fav.ico" width=40 height=40/>
+        </a>
         <button class="mr-3 navbar-toggler border-0" type="button" data-toggle="collapse"
             data-target="#hamburguinho" aria-controls="hamburguinho" aria-expanded="false"
             aria-label="Alterna navegação">
